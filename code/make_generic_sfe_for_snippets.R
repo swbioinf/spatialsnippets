58ac075c5b11ad3444e765c8bd9c3dd9d2b95242
swbioinf <s.williams@qcif.edu.au>
2025-11-07 04:14:58 +0000

    now with sfe/bioconductor DE

--- code/make_generic_sfe_for_snippets.R
+++ code/make_generic_sfe_for_snippets.R
@@ -7,7 +7,8 @@ library(Voyager)
 
 data_dir               <- file.path("~/projects/spatialsnippets/datasets/GSE234713_IBDcosmx_GarridoTrigo2023/processed_data")
 sfe_01_loaded          <- file.path(data_dir, "GSE234713_CosMx_IBD_sfe_01_loaded")
-sfe_file_01_generic <- file.path(data_dir, "GSE234713_CosMx_IBD_sfe_01_generic4snippettests.RDS")
+sfe_file_01_generic    <- file.path(data_dir, "GSE234713_CosMx_IBD_sfe_01_generic4snippettests")
+
 
 sfe <- readObject(sfe_01_loaded)
 
@@ -24,6 +25,30 @@ generic_group_names <- c(CD='GroupA', UC='GroupB', HC='Control')
 sfe$group     <- factor(as.character(generic_group_names[as.character(sfe$orig.group)]), levels=generic_group_names)
 
 saveObject(sfe, sfe_file_01_generic)
+
+
+################################################################################
+
+# Also, make a generic version of the pdb, since that takes so long
+se_pseudobulk_file.sample <- file.path(data_dir, "GSE234713_CosMx_IBD_pseudobulk_sample_se2.RDS") #### TEST _ Updated
+sfe_file_01_generic_min200_pdb <- file.path(data_dir, "GSE234713_CosMx_IBD_sfe_01_generic4snippettests_min200_pdb.RDS")
+
+
+se.pdb <- readRDS(se_pseudobulk_file.sample)
+
+
+# Make names 'generic'
+se.pdb$sample     <- as.factor(se.pdb$tissue_sample)
+se.pdb$individual <- as.factor(se.pdb$sample) # this is not a paried design, might never be useful.
+se.pdb$slide      <- as.factor(se.pdb$slide_name)
+se.pdb$cluster    <- as.factor(se.pdb$celltype_subset)
+se.pdb$celltype   <- as.factor(se.pdb$celltype_subset)
+
+se.pdb$orig.group <- se.pdb$group
+generic_group_names <- c(CD='GroupA', UC='GroupB', HC='Control')
+se.pdb$group     <- factor(as.character(generic_group_names[as.character(se.pdb$orig.group)]), levels=generic_group_names)
+
+saveRDS(se.pdb, file = sfe_file_01_generic_min200_pdb)
 
 
 
--- code/run_cosmxIBD_sfe_pseudobulk.R
+++ code/run_cosmxIBD_sfe_pseudobulk.R
@@ -13,7 +13,7 @@ library(BiocParallel)
 data_dir              <- file.path("~/projects/spatialsnippets/datasets/GSE234713_IBDcosmx_GarridoTrigo2023/processed_data")
 sfe_01_loaded <- file.path(data_dir, "GSE234713_CosMx_IBD_sfe_01_loaded")
 se_pseudobulk_file.fov    <- file.path(data_dir, "GSE234713_CosMx_IBD_pseudobulk_fov_se.RDS")
-se_pseudobulk_file.sample <- file.path(data_dir, "GSE234713_CosMx_IBD_pseudobulk_sample_se.RDS")
+se_pseudobulk_file.sample <- file.path(data_dir, "GSE234713_CosMx_IBD_pseudobulk_sample_se2.RDS") #### TEST _ Updated
 
 
 sfe <- readObject(sfe_01_loaded)
@@ -25,12 +25,12 @@ min_reads_per_cell <- 200
 
 sfe <- sfe[,sfe$total_count>= min_reads_per_cell]
 
-# at the FOV level
-sfe$pdb_sample <- paste0(sfe$tissue_sample, '_', sfe$fov, '_', sfe$celltype_subset)
-se.pdb <- aggregateAcrossCells(sfe, ids=sfe$pdb_sample,
-                               BPPARAM = MulticoreParam(workers=8) # Use 8 cores, requires BiocParallel
-)
-saveRDS(se.pdb, se_pseudobulk_file.fov)
+# # at the FOV level
+# sfe$pdb_sample <- paste0(sfe$tissue_sample, '_', sfe$fov, '_', sfe$celltype_subset)
+# se.pdb <- aggregateAcrossCells(sfe, ids=sfe$pdb_sample,
+#                                BPPARAM = MulticoreParam(workers=8) # Use 8 cores, requires BiocParallel
+# )
+# saveRDS(se.pdb, se_pseudobulk_file.fov)
 
 # At the sample elvel
 sfe$pdb_sample <- paste0(sfe$tissue_sample, '_', sfe$celltype_subset)
@@ -38,3 +38,5 @@ se.pdb <- aggregateAcrossCells(sfe, ids=sfe$pdb_sample,
                                BPPARAM = MulticoreParam(workers=8) # Use 8 cores, requires BiocParallel
 )
 saveRDS(se.pdb, se_pseudobulk_file.sample)
+
+print("Done.")
